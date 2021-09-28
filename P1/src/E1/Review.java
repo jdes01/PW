31c056a21e70d08861a5352f846960ddac4740fb
jdes01 <jasampa3000@gmail.com>
2021-09-28 14:15:19 +0200

    feat: review class init

---
+++ P1/src/E1/Review.java
@@ -0,0 +1,15 @@
+package E1;
+
+//import java.util.ArrayList;
+
+public class Review {
+
+    private String title;
+
+    private int rating;
+
+    private String review_text;
+
+    //private ArrayList<Pair<User, Integer>> users_ratings;s
+}
+

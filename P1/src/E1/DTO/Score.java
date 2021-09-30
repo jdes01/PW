a5129a615342c27f79e9edbe2e2f88da07ec9823
jdes01 <jasampa3000@gmail.com>
2021-09-30 20:45:01 +0200

    feat: score DTO created

---
+++ P1/src/E1/DTO/Score.java
@@ -0,0 +1,21 @@
+package E1.DTO;
+
+public class Score {
+    
+    Integer score;
+
+    public Score(Integer score){
+
+        this.score = score;
+    }
+
+    public Integer getScore(){
+
+        return score;
+    }
+
+    public void setScore(Integer score){
+
+        this.score = score;
+    }
+}

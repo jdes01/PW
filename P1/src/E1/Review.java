7642a4301a794513fd2fa647b59628a5b9ad250c
jdes01 <jasampa3000@gmail.com>
2021-09-30 20:58:48 +0200

    feat: user added to review class

--- P1/src/E1/Review.java
+++ P1/src/E1/Review.java
@@ -2,10 +2,14 @@ package E1;
 
 import java.util.ArrayList;
 
+import E1.DTO.User;
+
 import E1.DTO.Score;
 import E1.DTO.UserReview;
 
 public class Review {
+
+    private User user;
 
     private String title;
 
@@ -14,6 +18,6 @@ public class Review {
     private String review_text;
 
     private ArrayList<UserReview> user_reviews;
-    
+
 }
 

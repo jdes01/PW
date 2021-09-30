b8746850a29fd8fa9b2155d7bc5444b7cc026441
jdes01 <jasampa3000@gmail.com>
2021-09-30 20:45:56 +0200

    feat: review class created

--- P1/src/E1/Review.java
+++ P1/src/E1/Review.java
@@ -1,15 +1,19 @@
 package E1;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
+
+import E1.DTO.Score;
+import E1.DTO.UserReview;
 
 public class Review {
 
     private String title;
 
-    private int rating;
+    private Score score;
 
     private String review_text;
 
-    //private ArrayList<Pair<User, Integer>> users_ratings;s
+    private ArrayList<UserReview> user_reviews;
+    
 }
 

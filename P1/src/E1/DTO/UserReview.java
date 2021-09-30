ee5de28d0c52428046efe4fd81d1708eedbbf570
jdes01 <jasampa3000@gmail.com>
2021-09-30 20:45:25 +0200

    feat: userReview DTO created

---
+++ P1/src/E1/DTO/UserReview.java
@@ -0,0 +1,33 @@
+package E1.DTO;
+
+public class UserReview {
+    
+    private User user;
+    private Score score;
+
+    public UserReview(User user, Score score){
+
+        this.user = user;
+        this.score = score;
+    }
+
+    public Integer getUser(){
+
+        return this.user.getUser();
+    }
+
+    public void setUser(User user){
+
+        this.user = user;
+    }
+
+    public Integer getScore(){
+
+        return this.score.getScore();
+    }
+
+    public void setScore(Integer score){
+
+        this.score.setScore(score);
+    }
+}

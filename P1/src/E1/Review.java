8b6544e7d93cd3231a1f11e0e6a88cfbfd6ca857
jdes01 <jasampa3000@gmail.com>
2021-09-30 21:14:10 +0200

    feat: constructor added to Review class

--- P1/src/E1/Review.java
+++ P1/src/E1/Review.java
@@ -19,5 +19,14 @@ public class Review {
 
     private ArrayList<UserReview> user_reviews;
 
+
+    public Review(User user, String title, Integer score, String review_text){
+
+        this.user = user;
+        this.title = title;
+        this.score = new Score(score);
+        this.review_text = review_text;
+    }
+
 }
 

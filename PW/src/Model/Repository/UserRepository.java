d6108d72d8e9802fe340fad22bcc7980be7a2127
jdes01 <jasampa3000@gmail.com>
2021-10-07 10:04:55 +0200

    feat: first attempt of userRepository.saveUser method

--- PW/src/Model/Repository/UserRepository.java
+++ PW/src/Model/Repository/UserRepository.java
@@ -1,6 +1,30 @@
+import User;
+
+import org.json.simple.JSONObject;
+
+import java.io.File;  
+import java.io.FileWriter;  
+
 public class UserRepository {
+
+    private File userFile = new File("userFile.json");
     
-    public void saveUser(User user){} //TODO
+    public void saveUser(User user){
+
+        JSONObject userList = new JSONObject(); 
+
+        userList.put("name", user.name);
+
+        //todo add all atributes
+
+        userFile.createNewFile();  
+
+        FileWriter fileWriter = new FileWriter(userFile);  
+
+        fileWriter.write(userFile.toJSONString());  
+        fileWriter.flush();  
+        fileWriter.close();  
+    }
 
     public ArrayList<User> getUsers(){} //TODO
 

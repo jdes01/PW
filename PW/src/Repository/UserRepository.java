08ed0de2f188af8363e8cff16d6f0cc89d71f5cd
Marc <rodriguezmoreno.marcos@gmail.com>
2021-10-07 13:41:30 +0200

    added saveUser

--- PW/src/Repository/UserRepository.java
+++ PW/src/Repository/UserRepository.java
@@ -1,10 +1,45 @@
 package Repository;
 
-import Model.user;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import Model.Entities.User;
 
 public class UserRepository {
     
-    public insert(User user) {
+	private static FileWriter file;
+	
+	
+    public void saveUser(User user) throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("nick", user.getNick());
+        object.put("name", user.getName());
+        object.put("mail", user.getMail());
         
+        try {
+	        file = new FileWriter("./users.txt");
+	        file.write(object.toString());
+        } catch(IOException error) {
+        	error.printStackTrace();
+        } finally {
+        	try {
+        		file.flush();
+        		file.close();
+        	} catch(IOException error) {
+        		error.printStackTrace();
+        	}
+        }
     }
+    
+    public ArrayList<User> getUsers() {
+    	return null;
+	}
+    
+    public void deleteUser(User user) {}
+    
+    public User getUserByName(String name) {
+		return null;
+	}
 }

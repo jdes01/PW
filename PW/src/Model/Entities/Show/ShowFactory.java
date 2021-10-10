944e120f8bffbf8edaa11690bf3114988174e194
Marc <rodriguezmoreno.marcos@gmail.com>
2021-10-10 23:51:33 +0200

    small change

--- PW/src/Model/Entities/Show/ShowFactory.java
+++ PW/src/Model/Entities/Show/ShowFactory.java
@@ -14,6 +14,7 @@ import Model.Entities.Show.PunctualShow;
  *
  */
 public class ShowFactory implements Serializable {
+	/**
      * @param cathegory Categoria
      * @param description Descripcion
      * @param locationCapacity Capacidad

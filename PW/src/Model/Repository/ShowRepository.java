9851fbf3fcb7b9a453d511745369c8821d957d07
jdes01 <jasampa3000@gmail.com>
2021-10-10 18:49:25 +0200

    feat: showRepository methods saveShow and getShows

--- PW/src/Model/Repository/ShowRepository.java
+++ PW/src/Model/Repository/ShowRepository.java
@@ -1,12 +1,66 @@
+package Model.Repository;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import Model.Entities.Show.Show;
+
 public class ShowRepository {
  
-    public void saveShow(PunctualShow punctualShow){} //TODO
+    public void saveShow(Show show){
 
-    public void saveShow(SeasonShow seasonShow){} //TODO
+        try {
+        	FileOutputStream file = new FileOutputStream(new File("shows.txt"));
+        	ObjectOutputStream output = new ObjectOutputStream(file);
+        	
+        	output.writeObject(show);
+        	
+        	output.close();
+        	file.close();
+        } catch (FileNotFoundException error) {
+        	error.printStackTrace();
+        } catch (IOException error) {
+        	error.printStackTrace();
+        }
+    }
 
-    public void saveShow(MultiplePassShow multiplePassShow){} //TODO
-
-    public ArrayList<Show> getShows(); //TODO
+    @SuppressWarnings("null")
+	public ArrayList<Show> getShows(){
+    	ArrayList<Show> shows = null;
+    	
+    	try {
+	    	FileInputStream file = new FileInputStream(new File("shows.txt"));
+	    	ObjectInputStream input = new ObjectInputStream(file);
+	    	
+	    	while(true) {
+	    		try {
+	    			shows.add((Show) input.readObject());
+	    		} catch (EOFException error) {
+	    			input.close();
+	    			file.close();
+	    			return shows;
+	    		} catch (ClassNotFoundException error) {
+	    			input.close();
+	    			file.close();
+	    			error.printStackTrace();
+	    		}
+	    	}
+    	} catch (FileNotFoundException error) {
+        	error.printStackTrace();
+        } catch (IOException error) {
+        	error.printStackTrace();
+        }
+    	
+    	return shows;
+    }
 
     public void cancelShow(Show show);
 

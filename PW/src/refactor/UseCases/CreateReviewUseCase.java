68c0c5c1bcadc20c22f00151348f04eab682874b
Salsedini <d.s.022222@gmail.com>
2021-11-03 19:17:08 +0100

    CreateReviewUseCase

---
+++ PW/src/refactor/UseCases/CreateReviewUseCase.java
@@ -0,0 +1,22 @@
+package refactor.UseCases;
+
+import java.io.IOException;
+
+import refactor.Model.Entities.Review;
+import refactor.Model.Entities.User;
+import refactor.Model.Entities.Show.Show;
+import refactor.Model.ValueObjects.Score;
+import refactor.Repository.UserRepository;
+
+public class CreateReviewUseCase { 
+
+    public static void CreateReview(User user, String title, String text, Show show, Score score) throws IOException{
+
+    // ReviewRepository reviewRepository = new ReviewRepository();
+    //
+    // Review review = new Review(user, title, text, show, score);
+    // 
+    // reviewRepository.saveReview(review);
+    }
+    
+}

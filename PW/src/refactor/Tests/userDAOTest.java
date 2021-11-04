ec41cc3671839a68a9092a5e0009a2db7584f652
jdes01 <jasampa3000@gmail.com>
2021-11-04 19:45:00 +0100

    merge lil fix

--- PW/src/refactor/Tests/userDAOTest.java
+++ PW/src/refactor/Tests/userDAOTest.java
@@ -1,34 +1,5 @@
 package refactor.Tests;
 
-<<<<<<< HEAD
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.Test;
-
-import refactor.Model.Entities.User;
-import refactor.Repository.UserRepository;
-
-public class userDAOTest {
-    
-    @Test
-    void getUserByIDTest() throws IOException, ClassNotFoundException, SQLException, NullPointerException {
-
-        User user = new User("xxx", "xxx", "xxx", "xxx");
-        UserRepository userRepository = new UserRepository();
-        userRepository.saveUser(user);
-
-        User user2 = userRepository.getUserById(user.getUuid());
-
-        assertEquals(user2.getName(), user.getName());
-        assertEquals(user2.getLastName(), user.getLastName());
-        assertEquals(user2.getMail(), user.getMail());
-        assertEquals(user2.getNickName(), user.getNickName());
-        assertEquals(user2.getRole(), user.getRole());
-        assertEquals(user2.getUuid(), user.getUuid());
-=======
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
@@ -80,6 +51,5 @@ public class userDAOTest {
             System.out.println(users.get(i).getName() + " " + users.get(i).getUuid());
         }
 
->>>>>>> 0589e4d557dbea8d1f5ef4d6033ac0d471106d21
     }
 }

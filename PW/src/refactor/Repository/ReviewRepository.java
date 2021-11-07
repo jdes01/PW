2c731160c97eca77f206d7ae2e3225e223a162c3
jdes01 <jasampa3000@gmail.com>
2021-11-07 13:33:09 +0100

    feat: reviewRepository.saveReview() done

--- PW/src/refactor/Repository/ReviewRepository.java
+++ PW/src/refactor/Repository/ReviewRepository.java
@@ -1,5 +1,19 @@
 package refactor.Repository;
 
+import refactor.Model.Entities.Review;
+import refactor.Model.Entities.User;
+import refactor.Model.Entities.Show.Show;
+import refactor.Model.ValueObjects.Score;
+import refactor.Repository.DAOs.ReviewDAO;
+
 public class ReviewRepository {
+
+    public void saveReview(User user, String title, String text, Show show, Score score){
+
+        Review review = new Review(user, title, text, show, score);
+        ReviewDAO reviewDAO = new ReviewDAO();
+
+        reviewDAO.createReview(review);
+    }
     
 }

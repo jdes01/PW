c2c6af8c9a55f52099063b1ae70ca1a02f2a77eb
Salsedini <d.s.022222@gmail.com>
2021-11-08 16:55:15 +0100

    Documentación casos de uso

--- PW/src/refactor/UseCases/DeleteUserUseCase.java
+++ PW/src/refactor/UseCases/DeleteUserUseCase.java
@@ -4,9 +4,20 @@ import java.io.IOException;
 
 import refactor.Repository.UserRepository;
 
-
+/**
+ * Clase que se encarga de gestionar el caso de uso para llamar al repositorio y borrar un usuario concreto.
+ * 
+ * @author David Salcedo Sánchez 
+ */
 public class DeleteUserUseCase {
 
+    
+    /** 
+     * Caso de uso que se encarga de decirle a los repositorios de borrar un usuario especificado como parámetro.
+     * 
+     * @param mail el mail del usuario que servirá para especificar cuál es el usuario que se quiere eliminar.
+     * @throws IOException Signals that an I/O exception of some sort has occurred. This class is the general class of exceptions produced by failed or interrupted I/O operations.
+     */
     public static void deleteUser(String mail) throws IOException{
 
         UserRepository userRepository = new UserRepository();
--- PW/src/refactor/UseCases/RegisterAdminUseCase.java
+++ PW/src/refactor/UseCases/RegisterAdminUseCase.java
@@ -5,8 +5,31 @@ import java.sql.SQLException;
 
 import refactor.Repository.UserRepository;
 
+/**
+ * Caso de uso que se encarga de llamar al repositorio para registrar un usuario.
+ * 
+ * @author Marcos Rodríguez Moreno
+ */
 public class RegisterAdminUseCase { 
 
+    /** 
+     * Función dedicada al caso de uso que se encarga de llamar al repositorio para registrar un usuario.
+     * 
+     * @param name Nombre del usuario a registrar.
+     * @param lastName Apellido del usuario a registrar.
+     * @param nickName Apodo del usuario a registrar.
+     * @param mail Correo del usuario a registrar.
+     * @throws IOException Signals that an I/O exception of some sort has occurred. This class is the general class of exceptions produced by failed or interrupted I/O operations.
+     * @throws ClassNotFoundException Thrown when an application tries to load in a class through its string name using:
+
+        The forName method in class Class.
+        The findSystemClass method in class ClassLoader .
+        The loadClass method in class ClassLoader. 
+
+        but no definition for the class with the specified name could be found. 
+     * @throws SQLException An exception that provides information on a database access error or other errors.
+
+     */
     public static void registerAdmin(String name, String lastName, String nickName, String mail) throws IOException, ClassNotFoundException, SQLException{
 
         UserRepository userRepository = new UserRepository();

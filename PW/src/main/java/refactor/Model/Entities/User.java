b776d06da41695afa73230adefedc7bea13cc935
Alberto Cano <soniccano2@gmail.com>
2021-11-09 14:14:23 +0100

    feat: more classes and functions commented

--- PW/src/main/java/refactor/Model/Entities/User.java
+++ PW/src/main/java/refactor/Model/Entities/User.java
@@ -2,6 +2,12 @@ package refactor.Model.Entities;
 
 import java.util.UUID;
 
+/**
+ * Clase usuario que tiene todo lo relacionado a los usuarios
+ * 
+ * @author Marcos Rodriguez Moreno
+ */
+
 public class User {
 
     private final String admin = "ADMIN";
@@ -18,6 +24,15 @@ public class User {
 
     public User(){}
 
+   /**
+    * Constructor de la clase usuario
+
+    * @param name Nombre del usuario
+    * @param lastName Apellido del usuario
+    * @param nickName Nick del usuario
+    * @param mail Mail del usuario
+    */
+
     public User(String name, String lastName, String nickName, String mail){
 
         UUID uuid = UUID.randomUUID();
@@ -33,59 +48,132 @@ public class User {
 
 
 
+    
+    /** 
+     * Getter del ID
+     * 
+     * @return UUID
+     */
     public UUID getUuid(){
         return this._id;
     }
 
+    
+    /** 
+     * Getter del nombre
+     * 
+     * @return String
+     */
     public String getName() {
         return this._name;
     }
 
+    
+    /** 
+     * Getter del apellido
+     * 
+     * @return String
+     */
     public String getLastName() {
         return this._lastName;
     }
 
+    
+    /** 
+     * Getter del nick
+     * 
+     * @return String
+     */
     public String getNickName(){
         return this._nickName;
     }
 
+    
+    /** 
+     * Getter del mail
+     * 
+     * @return String
+     */
     public String getMail(){
         return this._mail;
     }
 
+    
+    /** 
+     * Getter del rol
+     * 
+     * @return String
+     */
     public String getRole(){
         return this._role;
     }
 
 
 
+    
+    /** 
+     * Setter del ID
+     * 
+     * @param id ID del usuario
+     */
     public void setID(String id) {
         this._id = UUID.fromString(id);
     }
 
+    
+    /** 
+     * Setter del nombre
+     * 
+     * @param name Nombre del usuario
+     */
     public void setName(String name) {
         this._name = name;
     }
 
+    
+    /** 
+     * Setter del apellido
+     * 
+     * @param lastName Apellido del usuario
+     */ 
     public void setLastName(String lastName) {
         this._lastName = lastName;
     }
 
+    
+    /** 
+     * Setter del nick
+     * 
+     * @param nickName Nick del usuario
+     */
     public void setNickName(String nickName){
         this._nickName = nickName;
     }
 
+    
+    /** 
+     * Setter del mail
+     * 
+     * @param mail Mail del usuario
+     */
     public void setMail(String mail){
         this._mail = mail;
     }
 
+    /**
+     * Setter del rol del administrador
+     */
+
     public void setRoleAdmin(){
         this._role = this.admin;
     }
+
+    /**
+     * Setter del rol del viewer
+     */
 
     public void setRoleViewer(){
         this._role = this.viewer;
     }
 
 }
-
--- PW/src/main/java/refactor/Model/ValueObjects/ShowSesion.java
+++ PW/src/main/java/refactor/Model/ValueObjects/ShowSesion.java
@@ -2,10 +2,23 @@ package refactor.Model.ValueObjects;
 
 import java.util.Calendar;
 
+/**
+ * Clase show session
+ * 
+ * @author Javier De Santiago Palomino
+ */
+
 public class ShowSesion {
 
     private Calendar _date;
     private int  _tickets;
+
+    /**
+     * Constructor de ShowSession
+     * 
+     * @param date Fecha del show
+     * @param tickets tickets del show
+     */
 
     public ShowSesion(Calendar date, int tickets){
 
@@ -13,10 +26,35 @@ public class ShowSesion {
         this._tickets = tickets;
     }
 
+/**
+ * Getter de la fecha
+ * 
+ * @return Calendar
+ */
+
     public Calendar getDate(){ return this._date; }
+
+/**
+ * Getter de los tickets
+ * 
+ * @return Integer
+ */
+
     public int getTickets(){ return this._tickets; }
 
+/**
+ * Setter de la fecha
+ * @param date Fecha del show
+ */
+
     public void setDate(Calendar date){ this._date = date; }
+
+/**
+ * Setter de los tickets
+ * 
+ * @param tickets Tickets del show
+ */
+
     public void setTickets(int tickets){ this._tickets = tickets; }
     
 }
